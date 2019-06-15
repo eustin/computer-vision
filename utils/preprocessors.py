a0d561bfffe464e15f02600d81072da8cc14e3c8
eustin <eustin.jvans@gmail.com>
2019-06-15 14:42:13 +1000

    complete image preprocessor which ignores aspect ratios

---
+++ utils/preprocessors.py
@@ -0,0 +1,13 @@
+
+import cv2
+
+class ImagePreprocessor:
+    def __init__(self, width, height, inter=cv2.INTER_AREA):
+        self.width = width
+        self.height = height
+        self.inter = inter
+
+    def process(self, image):
+        resized = cv2.resize(image, (self.widht, self.height), interpolation=self.inter)
+        return resized
+
