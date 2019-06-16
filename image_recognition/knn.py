1dea762b8d59dfe6a715054bcc8b01cd9d714172
eustin <eustin.jvans@gmail.com>
2019-06-16 16:15:36 +1000

    complete knn example. rename utils to compviz_utils

--- image_recognition/knn.py
+++ image_recognition/knn.py
@@ -1,10 +1,23 @@
+# knn on raw pixel arrays
+#
+# usage example:
+#   - cd into computer-vision root folder
+#   - in terminal:
+#
+#       python image_recognition/knn.py -d datasets/dogs-vs-cats
+#
+
+import sys
+import os
+utils_path = os.path.expanduser('~/github/computer-vision/')
+sys.path.append(utils_path)
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from utils.preprocessors import ImagePreprocessor
-from utils.data_loaders import DataLoader
-from imutils import paths
+from compviz_utils.preprocessors import ImagePreprocessor
+from compviz_utils.data_loaders import DataLoader
 import argparse
 
 ap = argparse.ArgumentParser()
@@ -16,6 +29,7 @@ ap.add_argument("-j", "--jobs", type=int, default=-1,
 args = vars(ap.parse_args())
 
 print("loading images...")
+
 # create preprocessors - resize images to 32x32 pixels
 preproc_resize = ImagePreprocessor(32, 32)
 
@@ -26,4 +40,19 @@ images, labels = data_loader.load('./datasets/dogs-vs-cats/')
 # flatten array into column vector
 # first axis (==0) is number of examples
 # second axis (==1) is elements in column vector
-images_flattened = images.reshape((images.shape[0], 3072))
+images_flattened = images.reshape((images.shape[0], 32*32*3))
+
+# encode target labels
+le = LabelEncoder()
+labels = le.fit_transform(labels)
+
+# train vs test split
+x_train, x_test, y_train, y_test = train_test_split(images_flattened, labels, test_size=0.25, random_state=42)
+
+# train model
+model = KNeighborsClassifier(n_neighbors=args['neighbours'], n_jobs=args['jobs'])
+model.fit(x_train, y_train)
+
+# evaluate
+results_report = classification_report(y_test, model.predict(x_test), target_names=le.classes_)
+print(results_report)
