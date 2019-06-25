86957b0ed06a4e922dedbc585f753590f6fd08e9
eustin <eustin.jvans@gmail.com>
2019-06-26 05:58:32 +1000

    rename compviz_utils to compviz. create nets folder under compviz

---
+++ compviz/preprocessors.py
@@ -0,0 +1,27 @@
+
+import cv2
+from keras.preprocessing.image import img_to_array
+
+
+class ImagePreprocessor:
+    def __init__(self, width, height, inter=cv2.INTER_AREA):
+        self.width = width
+        self.height = height
+        self.inter = inter
+
+    def process(self, image):
+        resized = cv2.resize(image, (self.width, self.height), interpolation=self.inter)
+        return resized
+
+
+class ImageToArray:
+    def __init__(self, data_format=None):
+        # data format here is either None, 'channels_first' or 'channels_last'
+        # if None, default setting of image_data_format in ~/.keras/keras.json will be used.
+        self.data_format = data_format
+
+
+    def preprocess(self, image):
+        return img_to_array(image, data_format=self.data_format)
+
+
