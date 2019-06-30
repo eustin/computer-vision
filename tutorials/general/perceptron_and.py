68c0488ebc7e28dc09f1b53516e8d8cde5211077
eustin <eustin.jvans@gmail.com>
2019-07-01 05:58:59 +1000

    fix comment

--- tutorials/general/perceptron_and.py
+++ tutorials/general/perceptron_and.py
@@ -8,7 +8,7 @@ y = np.array([[0], [0], [0], [1]])
 p = Perceptron(X.shape[1], alpha=0.1)
 p.fit(X, y, epochs=20)
 
-# test to see if we have learnt the 'or' function
+# test to see if we have learnt the 'and' function
 for x, target in zip(X, y):
     pred = p.predict(x)
     print("[INFO] data={}, ground-truth={}, pred={}".format(x, target[0], pred))
