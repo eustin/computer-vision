bcb407766a1313d2c5a52089c9993a041644cc4c
eustin <eustin.jvans@gmail.com>
2019-07-01 05:54:19 +1000

    complete example showing perceptron learning the 'OR' function.

---
+++ tutorials/general/perceptron_or.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+from tutorials.general.perceptron import Perceptron
+
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [1], [1], [1]])
+
+# create two weights + one for bias
+p = Perceptron(X.shape[1], alpha=0.1)
+p.fit(X, y, epochs=100)
+
+# test to see if we have learnt the 'or' function
+for x, target in zip(X, y):
+    pred = p.predict(x)
+    print("[INFO] data={}, ground-truth={}, pred={}".format(x, target[0], pred))
